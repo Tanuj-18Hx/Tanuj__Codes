07e1d0a9ec14b61c49c6faadc351b8fe6d3ee721
Tanuj-18Hx <tanjant1869@gmail.com>
2026-02-09 03:54:52 -0800

    Add files via upload

---
+++ InsurancePremiumCalculator.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class InsurancePremiumCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String type = sc.next();    
+        double amount = sc.nextDouble();
+        int claims = sc.nextInt();
+
+        double premium = 0;
+        double processingCharge = 0;
+        double discount = 0;
+
+        if (type.equals("Domestic")) { 
+            processingCharge = 50;
+            if (amount < 100000)
+                premium = amount * 0.003;
+            else
+                premium = amount * 0.0025;
+            if (claims == 0)
+                discount = 0.10;
+        } else if (type.equals("Commercial")) {
+            processingCharge = 80;
+            if (amount < 250000)
+                premium = amount * 0.005;
+            else
+                premium = amount * 0.0075;
+
+            if (claims == 0)
+                discount = 0.15;
+        }
+        double total = premium + processingCharge;
+        double finalPremium = total - (total * discount);
+        System.out.printf("%.2f", finalPremium);
+        sc.close();
+    }
+}
